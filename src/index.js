86146f0189c54a36125fbe201853ecbf97bdfd5c
Vincent Mac <vincent@simplicity.io>
2019-12-20 18:34:46 -0800

    chapter 2

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// 1
+// in SDL nomenclature , `info` is a "root field" 
 const typeDefs = `
 type Query {
 	info: String!
+	feed: [Link!]!
+}
+
+type Link {
+	id: ID!
+	description: String!
+	url: String!
 }
 `;
 
-// 2
+// 1
+let links = [{
+	id: 'link-0',
+	url: 'www.howtographql.com',
+	description: 'Fullstack tutorial for GraphQL'
+}];
+
 const resolvers = {
 	Query: {
-		info: () => `This is the API of a Hackernews Clone`
+		info: () => `This is the API of a Hackernews Clone`,
+		// 2
+		feed: () => links,
 		// info: () => null 
+	},
+	// 3
+	Link: {
+		id: (parent) => parent.id,
+		description: (parent) => parent.description,
+		url: (parent) => parent.url,
 	}
 };
 
-// 3
 const server = new GraphQLServer({
 	typeDefs,
 	resolvers,
