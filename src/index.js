a3216d0f7f90296e92fda5ab73cda19f3671b40b
Vincent Mac <vincent@simplicity.io>
2019-12-23 19:15:12 -0800

    finish chapter 2

--- src/index.js
+++ src/index.js
@@ -1,43 +1,73 @@
 const { GraphQLServer } = require('graphql-yoga');
 
-// in SDL nomenclature , `info` is a "root field" 
-const typeDefs = `
-type Query {
-	info: String!
-	feed: [Link!]!
-}
-
-type Link {
-	id: ID!
-	description: String!
-	url: String!
-}
-`;
-
-// 1
 let links = [{
 	id: 'link-0',
 	url: 'www.howtographql.com',
 	description: 'Fullstack tutorial for GraphQL'
 }];
 
+// 1
+let idCount = links.length;
+
+
+function getLinkById(id) {
+	for (let i = 0; i < links.length; i++) {
+		let link = links[i];
+		if (link.id == id) {
+			return link;
+		}
+	}
+}
+
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
-		// 2
 		feed: () => links,
 		// info: () => null 
+		link: (parent, args) => {
+			let {id} = args;
+			return getLinkById(id);
+		}
 	},
-	// 3
-	Link: {
-		id: (parent) => parent.id,
-		description: (parent) => parent.description,
-		url: (parent) => parent.url,
-	}
+	Mutation: {
+		// 2
+		post: (parent, args) => {
+			const link = {
+				id: `link-${idCount++}`,
+				description: args.description,
+				url: args.url,
+			};
+			links.push(link);
+			return link;
+		},
+		updateLink: (parent, args) => {
+			let {id, url, description} = args;
+			let link = getLinkById(id);
+
+			if (url) {
+				link.url  = url;
+			}
+
+			if (description) {
+				link.description  = description;
+			}
+			// TODO
+		},
+		deleteLink: (parent, args) => {
+			// TODO
+		},
+
+	},
+	// GraphQL.js will infer how it returns these fields based on the field name
+	// Link: {
+	// 	id: (parent) => parent.id,
+	// 	description: (parent) => parent.description,
+	// 	url: (parent) => parent.url,
+	// }
 };
 
 const server = new GraphQLServer({
-	typeDefs,
+	typeDefs: './src/schema.graphql',
 	resolvers,
 });
 
