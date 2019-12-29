a702059b96f8799c579a4f26dccd652b59444333
Vincent Mac <vincent@simplicity.io>
2019-12-28 20:55:18 -0800

    added authentication and jwt for authorization

--- src/index.js
+++ src/index.js
@@ -1,84 +1,27 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
 
-let links = [{
-	id: 'link-0',
-	url: 'www.howtographql.com',
-	description: 'Fullstack tutorial for GraphQL'
-}];
-
-// 1
-let idCount = links.length;
-
-
-function getLinkById(id) {
-	for (let i = 0; i < links.length; i++) {
-		let link = links[i];
-		if (link.id == id) {
-			return link;
-		}
-	}
-}
-
-function persistLink(link) {
-	for (let i = 0;  i < links.length; i++) {
-		let l = links[i];
-		if (l.id == link.id) {
-			links[i] = link;
-		}
-	}
-}
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
-	Query: {
-		info: () => `This is the API of a Hackernews Clone`,
-		feed: () => links,
-		// info: () => null
-		link: (_parent, args) => {
-			let {id} = args;
-			return getLinkById(id);
-		}
-	},
-	Mutation: {
-		// 2
-		post: (_parent, args) => {
-			const link = {
-				id: `link-${idCount++}`,
-				description: args.description,
-				url: args.url,
-			};
-			links.push(link);
-			return link;
-		},
-		updateLink: (_parent, args) => {
-			let {id, url, description} = args;
-			let link = getLinkById(id);
-
-			if (url) {
-				link.url  = url;
-			}
-
-			if (description) {
-				link.description  = description;
-			}
-
-			persistLink(link)
-		},
-		deleteLink: (parent, args) => {
-			// TODO
-		},
-
-	},
-	// GraphQL.js will infer how it returns these fields based on the field name
-	// Link: {
-	// 	id: (parent) => parent.id,
-	// 	description: (parent) => parent.description,
-	// 	url: (parent) => parent.url,
-	// }
+	Query,
+	Mutation,
+	User,
+	Link,
 };
 
 const server = new GraphQLServer({
 	typeDefs: './src/schema.graphql',
 	resolvers,
+	context: request => {
+		return {
+			...request,
+			prisma,
+		}
+	},
 });
 
 server.start(() => console.log(`Server is running on localhost:4000`)); // http://penguin.linux.test:4000/
