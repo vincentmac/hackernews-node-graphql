0e8147e306304d20d7d036183a92359eadcb7b46
Vincent Mac <vincent@simplicity.io>
2019-12-31 17:08:42 -0800

    add filtering, pagination, and sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,28 @@
-function feed(parent, args, context, info) {
-	return context.prisma.links()
+async function feed(parent, args, context, info) {
+	const where = args.filter ? {
+		OR: [
+			{ description_contains: args.filter },
+			{ url_contains: args.filter },
+		]
+	} : {};
+
+	const links = await context.prisma.links({
+		where,
+		skip: args.skip, // the `start index`, defaults to 0
+		first: args.first, // limit: retrieving `first` x elements after a the start index
+		orderBy: args.orderBy,
+	});
+
+	const count = await context.prisma
+		.linksConnection({
+			where,
+		})
+		.aggregate()
+		.count();
+	return {
+		links,
+		count,
+	}
 }
 
 module.exports = {
